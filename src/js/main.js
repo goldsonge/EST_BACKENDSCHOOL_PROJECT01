402b885fd8971d97e09586cc3a7a533b612e8909
goldsonge <ty7968@gmail.com>
2024-08-20 18:22:59 +0900

    240819 PC 퍼블리싱 완료 / 스크롤 탑 버튼 완료 / 이메일 유효성 검사 작업중

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,47 @@
+// header active
 window.addEventListener('scroll', function() {
     const header = document.querySelector('.header');
+    const btn = document.querySelector('.btn_top');
     if (window.scrollY > 0) {
         header.classList.add('sticky');
+        btn.classList.add('visible');
     } else {
         header.classList.remove('sticky');
+        btn.classList.remove('visible');
     }
 });
+
+// scroll top button
+const btnTop = document.querySelector('.btn_top')
+btnTop.addEventListener('click', function(event) {
+    event.preventDefault();
+    scrollToTop();
+});
+
+function scrollToTop() {
+    const scrollStep = window.scrollY / 15;
+    const scrollInterval = setInterval(function() {
+        if (window.scrollY !== 0) {
+            window.scrollBy(0, -scrollStep);
+        } else {
+            clearInterval(scrollInterval);
+        }
+    }, 15);
+}
+
+
+// 이메일 유효성 검사
+function join_user(){
+    event.preventDefault();
+    var useremail = document.getElementById("useremail");
+    var valid_txt = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+    //(알파벳,숫자)@(알파벳,숫자).(알파벳,숫자)
+
+    if(valid_txt.test(useremail.value)==false){
+
+        alert("이메일 주소가 올바르지 않습니다.");
+        useremail.focus();
+        return;
+    }
+
+}

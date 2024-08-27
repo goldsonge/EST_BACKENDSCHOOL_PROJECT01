e42f58d7ed5e12704f3d252bd0194d4bb200bc51
goldsonge <ty7968@gmail.com>
2024-08-27 09:25:20 +0900

    240827 주석 내용 삭제 및 수정

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
 window.addEventListener('scroll', function() {
     const header = document.querySelector('.header');
     const btn = document.querySelector('.btn_top');
+
     if (window.scrollY > 0) {
         header.classList.add('sticky');
         btn.classList.add('visible');
@@ -34,8 +35,8 @@ function scrollToTop() {
 function join_user(){
     event.preventDefault();
     const useremail = document.getElementById("useremail");
-    const validTxt = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
-    //(알파벳,숫자)@(알파벳,숫자).(알파벳,숫자)
+    const validTxt = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/; //(알파벳,숫자)@(알파벳,숫자).(알파벳,숫자)
+
 
     if(validTxt.test(useremail.value)==false){
         alert("이메일 주소가 올바르지 않습니다.");
@@ -43,7 +44,6 @@ function join_user(){
         return;
     } else {
         // 이메일 주소를 올바르게 작성했을 때
-        // console.log(useremail.value)
 
         const body = document.body;
         const form = document.getElementById("myForm");
@@ -51,9 +51,10 @@ function join_user(){
         const popBg = document.querySelector('.sec05 .pop_wr .pop_bg');
         const popBtn = document.querySelector('.sec05 .pop_wr .exit_btn');
 
-        function togglePopup(isActive) {
-            body.classList.toggle('scrollLock', isActive);
-            popCont.classList.toggle('active', isActive);
+        // 클래스 추가
+        function togglePopup() {
+            body.classList.toggle('scrollLock');
+            popCont.classList.toggle('active');
         }
         // 팝업 버튼 클릭시
         popBtn.addEventListener('click', function(event) {
